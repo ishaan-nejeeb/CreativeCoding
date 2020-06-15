0142939f8d4b69cd9b4e79a2c86c1ea7e65277f0
ishaan-nejeeb <nejeeb.ishaan@gmail.com>
2020-06-15 19:04:59 +0530

    dipole started

--- flow_field_bg/flow_field_bg.pde
+++ flow_field_bg/flow_field_bg.pde
@@ -1,27 +1,69 @@
 FlowField flowfield;
+Dipole pole;
 ArrayList<Particle> particles;
+PVector l;
 
 //boolean debug = true;
 
 void setup() {
   size(800, 800);
+
+  l = new PVector(400, 400);
   
   flowfield = new FlowField(20);
   flowfield.update();
+
+  pole = new Dipole(l, 20);
+  pole.update();
 
 //   particles = new ArrayList<Particle>();
 //   for (int i = 0; i < 1000; i++) {
 //     PVector start = new PVector(random(width), random(height));
 //     particles.add(new Particle(start, random(2, 8)));
 //   }
+
   background(0);
+
+//   ellipse(400, 400, 10, 10);
+
+
+//   for (int y = 0; y < flowfield.rows; y++) {
+//             for (int x = 0; x < flowfield.cols; x++) {
+//                 int index = x + y * flowfield.cols;
+//                 PVector v = flowfield.vectors[index];
+
+//                 float dist = v.dist(pole);
+//                 float force = 900 / (dist * dist);
+//                 ///PVector ab = PVector.sub(pole, v);
+//                 float ang = PVector.angleBetween(v, pole);
+//                 PVector temp = PVector.fromAngle(ang);
+//                 temp.normalize();
+//                 temp.mult(force);
+//                 v.add(temp);
+
+//                 stroke(255);
+//                 strokeWeight(1);
+//                 pushMatrix();
+//                 translate(x * flowfield.scl, y * flowfield.scl);
+//                 //fill(map(angle,0,2*TWO_PI,0,255));
+//                 //rect(0, 0, scl, scl);
+//                 rotate(v.heading());
+//                 line(0, 0, flowfield.scl, 0);
+//                 popMatrix();
+//             }
+//         }
 }
 
+
+  //flowfield.display();
+
+
 void draw() {
-  flowfield.update();
+  //flowfield.update();
   
   //if (debug)
-  flowfield.display();
+  //flowfield.display();
+  pole.display();
   
 //   for (Particle p : particles) {
 //     p.follow(flowfield);
--- flow_field_bg/out/source/flow_field_bg.java
+++ flow_field_bg/out/source/flow_field_bg.java
@@ -15,34 +15,137 @@ import java.io.IOException;
 public class flow_field_bg extends PApplet {
 
 FlowField flowfield;
+Dipole pole;
 ArrayList<Particle> particles;
+PVector l;
 
 //boolean debug = true;
 
 public void setup() {
   
+
+  l = new PVector(400, 400);
   
   flowfield = new FlowField(20);
   flowfield.update();
+
+  pole = new Dipole(l, 20);
+  pole.update();
 
 //   particles = new ArrayList<Particle>();
 //   for (int i = 0; i < 1000; i++) {
 //     PVector start = new PVector(random(width), random(height));
 //     particles.add(new Particle(start, random(2, 8)));
 //   }
+
   background(0);
+
+//   ellipse(400, 400, 10, 10);
+
+
+//   for (int y = 0; y < flowfield.rows; y++) {
+//             for (int x = 0; x < flowfield.cols; x++) {
+//                 int index = x + y * flowfield.cols;
+//                 PVector v = flowfield.vectors[index];
+
+//                 float dist = v.dist(pole);
+//                 float force = 900 / (dist * dist);
+//                 ///PVector ab = PVector.sub(pole, v);
+//                 float ang = PVector.angleBetween(v, pole);
+//                 PVector temp = PVector.fromAngle(ang);
+//                 temp.normalize();
+//                 temp.mult(force);
+//                 v.add(temp);
+
+//                 stroke(255);
+//                 strokeWeight(1);
+//                 pushMatrix();
+//                 translate(x * flowfield.scl, y * flowfield.scl);
+//                 //fill(map(angle,0,2*TWO_PI,0,255));
+//                 //rect(0, 0, scl, scl);
+//                 rotate(v.heading());
+//                 line(0, 0, flowfield.scl, 0);
+//                 popMatrix();
+//             }
+//         }
 }
 
+
+  //flowfield.display();
+
+
 public void draw() {
-  flowfield.update();
+  //flowfield.update();
   
   //if (debug)
-  flowfield.display();
+  //flowfield.display();
+  pole.display();
   
 //   for (Particle p : particles) {
 //     p.follow(flowfield);
 //     p.run();
 //   }
+}
+public class Dipole{
+    PVector location;
+    PVector[] vectors;
+    int scl;
+    int cols, rows;
+
+
+    Dipole(PVector loc, int res){
+        location = loc;
+        scl = res;
+        cols = floor(width/res) + 1;
+        rows = floor(height/res) + 1;
+        vectors = new PVector[cols*rows];
+
+    }
+
+    public void update(){
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                int index = x + y * cols;
+                PVector t = new PVector(1, 0);
+
+                PVector temp = new PVector(x * scl + scl / 2, y * scl + scl / 2);
+                float dist = PVector.dist(location, temp);
+                float force = 3600 / (dist * dist);
+                PVector ab = new PVector(location.x - (x * scl + scl / 2), location.y - (y * scl + scl / 2));
+                ab.normalize();
+                ab.mult(force);
+                ab.add(t);
+                float ang = PVector.angleBetween(temp, location);
+                PVector v = PVector.fromAngle(ang);
+                v.mult(force);
+                
+                vectors[index] = ab;
+    
+            }
+        }
+    }
+
+    public void display(){
+
+        ellipse(location.x - scl / 2, location.y - scl / 2, scl, scl);
+
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                int index = x + y * cols;
+                PVector v = vectors[index];
+
+                stroke(255);
+                strokeWeight(1);
+                pushMatrix();
+                translate(x * scl, y * scl);
+                //fill(map(angle,0,2*TWO_PI,0,255));
+                //rect(0, 0, scl, scl);
+                rotate(v.heading());
+                line(0, 0, scl, 0);
+                popMatrix();
+            }
+        }
+    }
 }
 public class FlowField{
     PVector[] vectors;
