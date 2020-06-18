7ecb2168f62b10b8891b04b941514f0f0f25e342
ishaan-nejeeb <nejeeb.ishaan@gmail.com>
2020-06-19 03:54:33 +0530

    completed without dipoles

--- flow_field_2/flow_field_2.pde
+++ flow_field_2/flow_field_2.pde
@@ -1,82 +1,126 @@
 import processing.svg.*;
+import processing.pdf.*;
 
 
 
 int drawLength = 150;
-float noiseScale = 0.05;
+float noiseScale = 0.03;
 int resolution = 20;
-int strokeLength = 40;
+int strokeLength = 25;
 int layers = 8;
-float zInc = 0.001;
+float zInc = 0.0000;
 float zNoiseScale = 0;
+PVector[] poles;
+int numPoles = 10;
 
+int frame = 0;
+int l = 0;
 
-int frame;
-int l;
+//--------------------------------------------------
+//colors
+int minH = 359;
+int maxH = 360;
+int minS = 0;
+int maxS = 1;
+int minL = 0;
+int maxL = 10;
+//--------------------------------------------------
+void mouseClicked() {
+    saveFrame("19.png");
+}
+
+boolean record;
 
 
 void setup() {
     size(1000, 1000);
     background(255);
-    load();
-    colorMode(HSB, 360, 100, 100);
+    int prev = 0;
+    //load();
+    poles = new PVector[numPoles];
+    for (int i = 0; i < numPoles; ++i) {
+        int x, y, z;
+        z = ((i % 2) == 1) ? 1 : -1;
+        y = ((i % 2) == 1) ? prev + 10 : int(random(height));
+        x = int(random(width));
+        PVector temp = new PVector(x, y, z);
+        poles[i] = temp;
+        prev = int(poles[i].y);
+    }
+
+    //poles[0] = new PVector(500, 500, 1);
+    //poles[1] = new PVector(500, 510, -1);
+    //colorMode(HSB, 360, 100, 100);
+
+    //beginRecord(SVG, "1.svg");
+    //beginRecord(PDF, "1.pdf");
+    
 
 }
 
 
 void draw() {
-    //for (int l = 0; l < layers; ++l) {
-        if (l > layers) {
+
+    // if (l == layers){
+    //     beginRecord(SVG, "frame-####.svg");    
+    // }
+
+    if (l > layers) {
+        //endRecord();
         return;
+        //exit();colorMode(HSB, 360, 100, 100);
+    }
+    colorMode(HSB, 360, 100, 100);
+    frame = 0;
+    zNoiseScale = 0;
+
+    while (frame <= drawLength) {
+        int count = int(map(frame, 0, drawLength, 2, 80));
+
+        for (int i = 0; i < count; ++i) {
+            push();
+            translate(50,50);
+            int x = int(random(900));
+            int y = int(random(900));
+
+            int resX = floor(x / resolution);;
+            int resY = floor(y / resolution);;
+
+            int index = (resY * 900 / resolution) + resX;
+
+            //stroke(280, 100, 14, 5);
+
+            int sw = int(map(frame, 0, drawLength, 8, 6));
+            strokeWeight(sw);
+
+            push();
+            translate(x, y);
+
+            float n = noise(resX * noiseScale, resY * noiseScale, zNoiseScale);
+            float totalAngle = dipole(n, x, y);
+            rotate(radians(map(n, 0, 1, -180, 180)));
+            //rotate(totalAngle);
+
+            stroke(setColor(n), int(random(minS, maxS)), int(random(minL, maxL)), 255);
+
+            float lengthVariation = random(0.8, 1.20);
+            line(0, 0, strokeLength * lengthVariation, 0);
+
+            //stroke(random(255), random(255), random(255), random(100));
+            strokeWeight(sw * 0.8);
+            line(0, -sw * 0.15, strokeLength * lengthVariation, -sw * 0.15);
+            pop();
+            pop();
         }
-        
-        frame = 0;
-        zNoiseScale = 0;
 
-        while (frame <= drawLength) {
-            int count = int(map(frame, 0, drawLength, 2, 80));
-
-            for (int i = 0; i < count; ++i) {
-                int x = int(random(width));
-                int y = int(random(height));
-
-                int resX = floor(x / resolution);;
-                int resY = floor(y / resolution);;
-
-                int index = (resY * width / resolution) + resX;
-
-                //stroke(280, 100, 14, 5);
-
-                int sw = int(map(frame, 0, drawLength, 10, 5));
-                strokeWeight(sw);
-
-                push();
-                translate(x, y);
-
-                float n = noise(resX * noiseScale, resY * noiseScale, zNoiseScale);
-                rotate(radians(map(n, 0, 1, -180, 180)));
-
-                stroke(setColor(n), int(random(90, 100)), int(random(5, 35)), 50);
-
-                float lengthVariation = random(0.5, 1.75);
-                line(0, 0, strokeLength * lengthVariation, 0);
-
-                //stroke(random(255), random(255), random(255), random(100));
-                strokeWeight(sw * 0.8);
-                line(0, -sw * 0.15, strokeLength * lengthVariation, -sw * 0.15);
-                pop();
-            }
-
-            frame++;
-            zNoiseScale += zInc;
-        }
-
-        l++;
-        
-        
-
-    //}
-   //frame = 0;
+        frame++;
+        zNoiseScale += zInc;
+    }
+    // if (l == layers) {
+    //     endRecord();
+    // }
+    l++;
+    
 }
 
 void load() {
@@ -90,6 +134,71 @@ void load() {
 
 int setColor(float n){
 
-    return int(map(n, 0, 1, 100, 330));
+    //return int(map(n, 0, 1, minH, maxH));
+    int col =  int(map(n, 0, 1, minH, maxH));
+    col = col > 360 ? col % 360 : col;
+    return col;
 
 }
+
+
+float dipole(float noise, int x, int y){
+    PVector loc = new PVector(x, y);
+    PVector toPole = new PVector(0, 0);
+    float distance;
+    float force;
+    PVector noiseForce;
+    PVector totalForce = new PVector(0, 0);
+    float minForce = 0.25;
+
+    float angle1 = radians(map(noise, 0, 1, -180, 180));
+    noiseForce = PVector.fromAngle(angle1);
+    noiseForce.setMag(noise);
+
+    totalForce.add(noiseForce);
+
+    for (int i = 0; i < numPoles; i++) {
+        toPole.x = 0;
+        toPole.y = 0;
+
+        distance = PVector.dist(loc, poles[i]);
+        force = 62500 / (distance * distance);
+
+        if (force < minForce) {
+            force = 0;
+            toPole.mult(0);
+        }
+        if (force != 0) {
+            //force = map(force, minForce, 3600, 0, 1);   
+            if (poles[i].z == 1){
+                toPole = PVector.sub(poles[i], loc);
+            } else {
+                toPole = PVector.sub(loc, poles[i]);
+            }
+            
+            toPole.setMag(force); 
+            //toPole.mult(poles[i].z);
+        }
+
+        
+
+        totalForce.add(toPole);
+
+    }
+
+    //float finalAngle = totalForce.heading();//radians(map(totalForce.heading(), 0, numPoles + 1, -180, 180));
+    //return finalAngle;     
+    //print(totalForce.heading());
+    return totalForce.heading();   
+    //return toPole.heading();
+}
+
+
+
+// void keyPressed() {
+//   if (key == 'q') {
+//     endRecord();
+//     exit();
+//     //record = true;
+//   }
+// }
--- flow_field_2/out/source/flow_field_2.java
+++ flow_field_2/out/source/flow_field_2.java
@@ -4,6 +4,7 @@ import processing.event.*;
 import processing.opengl.*; 
 
 import processing.svg.*; 
+import processing.pdf.*; 
 
 import java.util.HashMap; 
 import java.util.ArrayList; 
@@ -20,81 +21,125 @@ public class flow_field_2 extends PApplet {
 
 
 
+
 int drawLength = 150;
-float noiseScale = 0.05f;
+float noiseScale = 0.03f;
 int resolution = 20;
-int strokeLength = 40;
+int strokeLength = 25;
 int layers = 8;
-float zInc = 0.001f;
+float zInc = 0.0000f;
 float zNoiseScale = 0;
+PVector[] poles;
+int numPoles = 10;
 
+int frame = 0;
+int l = 0;
 
-int frame;
-int l;
+//--------------------------------------------------
+//colors
+int minH = 359;
+int maxH = 360;
+int minS = 0;
+int maxS = 1;
+int minL = 0;
+int maxL = 10;
+//--------------------------------------------------
+public void mouseClicked() {
+    saveFrame("19.png");
+}
+
+boolean record;
 
 
 public void setup() {
     
     background(255);
-    load();
-    colorMode(HSB, 360, 100, 100);
+    int prev = 0;
+    //load();
+    poles = new PVector[numPoles];
+    for (int i = 0; i < numPoles; ++i) {
+        int x, y, z;
+        z = ((i % 2) == 1) ? 1 : -1;
+        y = ((i % 2) == 1) ? prev + 10 : PApplet.parseInt(random(height));
+        x = PApplet.parseInt(random(width));
+        PVector temp = new PVector(x, y, z);
+        poles[i] = temp;
+        prev = PApplet.parseInt(poles[i].y);
+    }
+
+    //poles[0] = new PVector(500, 500, 1);
+    //poles[1] = new PVector(500, 510, -1);
+    //colorMode(HSB, 360, 100, 100);
+
+    //beginRecord(SVG, "1.svg");
+    //beginRecord(PDF, "1.pdf");
+    
 
 }
 
 
 public void draw() {
-    //for (int l = 0; l < layers; ++l) {
-        if (l > layers) {
+
+    // if (l == layers){
+    //     beginRecord(SVG, "frame-####.svg");    
+    // }
+
+    if (l > layers) {
+        //endRecord();
         return;
+        //exit();colorMode(HSB, 360, 100, 100);
+    }
+    colorMode(HSB, 360, 100, 100);
+    frame = 0;
+    zNoiseScale = 0;
+
+    while (frame <= drawLength) {
+        int count = PApplet.parseInt(map(frame, 0, drawLength, 2, 80));
+
+        for (int i = 0; i < count; ++i) {
+            push();
+            translate(50,50);
+            int x = PApplet.parseInt(random(900));
+            int y = PApplet.parseInt(random(900));
+
+            int resX = floor(x / resolution);;
+            int resY = floor(y / resolution);;
+
+            int index = (resY * 900 / resolution) + resX;
+
+            //stroke(280, 100, 14, 5);
+
+            int sw = PApplet.parseInt(map(frame, 0, drawLength, 8, 6));
+            strokeWeight(sw);
+
+            push();
+            translate(x, y);
+
+            float n = noise(resX * noiseScale, resY * noiseScale, zNoiseScale);
+            float totalAngle = dipole(n, x, y);
+            rotate(radians(map(n, 0, 1, -180, 180)));
+            //rotate(totalAngle);
+
+            stroke(setColor(n), PApplet.parseInt(random(minS, maxS)), PApplet.parseInt(random(minL, maxL)), 255);
+
+            float lengthVariation = random(0.8f, 1.20f);
+            line(0, 0, strokeLength * lengthVariation, 0);
+
+            //stroke(random(255), random(255), random(255), random(100));
+            strokeWeight(sw * 0.8f);
+            line(0, -sw * 0.15f, strokeLength * lengthVariation, -sw * 0.15f);
+            pop();
+            pop();
         }
-        
-        frame = 0;
-        zNoiseScale = 0;
 
-        while (frame <= drawLength) {
-            int count = PApplet.parseInt(map(frame, 0, drawLength, 2, 80));
-
-            for (int i = 0; i < count; ++i) {
-                int x = PApplet.parseInt(random(width));
-                int y = PApplet.parseInt(random(height));
-
-                int resX = floor(x / resolution);;
-                int resY = floor(y / resolution);;
-
-                int index = (resY * width / resolution) + resX;
-
-                //stroke(280, 100, 14, 5);
-
-                int sw = PApplet.parseInt(map(frame, 0, drawLength, 10, 5));
-                strokeWeight(sw);
-
-                push();
-                translate(x, y);
-
-                float n = noise(resX * noiseScale, resY * noiseScale, zNoiseScale);
-                rotate(radians(map(n, 0, 1, -180, 180)));
-
-                stroke(setColor(n), PApplet.parseInt(random(90, 100)), PApplet.parseInt(random(5, 35)), 50);
-
-                float lengthVariation = random(0.5f, 1.75f);
-                line(0, 0, strokeLength * lengthVariation, 0);
-
-                //stroke(random(255), random(255), random(255), random(100));
-                strokeWeight(sw * 0.8f);
-                line(0, -sw * 0.15f, strokeLength * lengthVariation, -sw * 0.15f);
-                pop();
-            }
-
-            frame++;
-            zNoiseScale += zInc;
-        }
-
-        l++;
-        
-        
-
-    //}
-   //frame = 0;
+        frame++;
+        zNoiseScale += zInc;
+    }
+    // if (l == layers) {
+    //     endRecord();
+    // }
+    l++;
+    
 }
 
 public void load() {
@@ -108,9 +153,74 @@ public void load() {
 
 public int setColor(float n){
 
-    return PApplet.parseInt(map(n, 0, 1, 100, 330));
+    //return int(map(n, 0, 1, minH, maxH));
+    int col =  PApplet.parseInt(map(n, 0, 1, minH, maxH));
+    col = col > 360 ? col % 360 : col;
+    return col;
 
 }
+
+
+public float dipole(float noise, int x, int y){
+    PVector loc = new PVector(x, y);
+    PVector toPole = new PVector(0, 0);
+    float distance;
+    float force;
+    PVector noiseForce;
+    PVector totalForce = new PVector(0, 0);
+    float minForce = 0.25f;
+
+    float angle1 = radians(map(noise, 0, 1, -180, 180));
+    noiseForce = PVector.fromAngle(angle1);
+    noiseForce.setMag(noise);
+
+    totalForce.add(noiseForce);
+
+    for (int i = 0; i < numPoles; i++) {
+        toPole.x = 0;
+        toPole.y = 0;
+
+        distance = PVector.dist(loc, poles[i]);
+        force = 62500 / (distance * distance);
+
+        if (force < minForce) {
+            force = 0;
+            toPole.mult(0);
+        }
+        if (force != 0) {
+            //force = map(force, minForce, 3600, 0, 1);   
+            if (poles[i].z == 1){
+                toPole = PVector.sub(poles[i], loc);
+            } else {
+                toPole = PVector.sub(loc, poles[i]);
+            }
+            
+            toPole.setMag(force); 
+            //toPole.mult(poles[i].z);
+        }
+
+        
+
+        totalForce.add(toPole);
+
+    }
+
+    //float finalAngle = totalForce.heading();//radians(map(totalForce.heading(), 0, numPoles + 1, -180, 180));
+    //return finalAngle;     
+    //print(totalForce.heading());
+    return totalForce.heading();   
+    //return toPole.heading();
+}
+
+
+
+// void keyPressed() {
+//   if (key == 'q') {
+//     endRecord();
+//     exit();
+//     //record = true;
+//   }
+// }
   public void settings() {  size(1000, 1000); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "flow_field_2" };
